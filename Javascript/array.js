b29760da4bb11c266ee1cede07766f720abb97a8
rajukunarapu <rajukunarapu49@gmail.com>
2025-03-26 01:16:47 +0530

    changes

--- Javascript/array.js
+++ Javascript/array.js
@@ -14,6 +14,12 @@
 // arr.shift()  // remove item from starting of an array
 // console.log(arr)
 
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(numbers[0, 2])
+console.log(numbers.slice(1, 5))
+console.log(numbers.splice(0, 3, ["raju"]))
+console.log(numbers)
+
 
 // //for in loop returns the index
 // for (index in arr) {
@@ -33,36 +39,36 @@
 // })
 
 
-// map, filter and reduce are special methods of array
-let arr1 = [1, 2, 3, 4, 5, 6]
+// // map, filter and reduce are special methods of array
+// let arr1 = [1, 2, 3, 4, 5, 6]
 
-let double = arr1.map((item, ind, tArr) => {
-    return item * 2
-})
-console.log(double)
+// let double = arr1.map((item, ind, tArr) => {
+//     return item * 2
+// })
+// console.log(double)
 
 
-let even_number = arr1.filter((item) => {
-    return (item % 2 === 0)
-}
-)
-console.log(even_number)
+// let even_number = arr1.filter((item) => {
+//     return (item % 2 === 0)
+// }
+// )
+// console.log(even_number)
 
-let sum = arr1.reduce((acc, item) => {
-    return item * acc
-}, 1)
-console.log(sum)
+// let sum = arr1.reduce((acc, item) => {
+//     return item * acc
+// }, 1)
+// console.log(sum)
 
-// find - It retuns the first item that's met the condition
-let arr_1 = [1, 2, 3, 4, 5, 6, 7]
-let finding_even = arr1.find((item) => item % 2 == 0)
-console.log(finding_even)
+// // find - It retuns the first item that's met the condition
+// let arr_1 = [1, 2, 3, 4, 5, 6, 7]
+// let finding_even = arr1.find((item) => item % 2 == 0)
+// console.log(finding_even)
 
-// some - It returns true if any one of the item is met condition
-let find_even = arr_1.some((item) => item % 2 == 0)
-console.log(find_even)
+// // some - It returns true if any one of the item is met condition
+// let find_even = arr_1.some((item) => item % 2 == 0)
+// console.log(find_even)
 
-// every - It returns true if all items meet the condition
-let student_marks = [38, 41, 57, 71, 68]
-let student_result = student_marks.every((item) => item > 36)
-console.log(student_result)
+// // every - It returns true if all items meet the condition
+// let student_marks = [38, 41, 57, 71, 68]
+// let student_result = student_marks.every((item) => item > 36)
+// console.log(student_result)
